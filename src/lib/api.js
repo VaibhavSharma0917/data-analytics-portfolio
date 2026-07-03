63b4a9e53e270d6db9b89f9deded2eea04b8a1f3
Vaibhav Sharma <17vsvaibhav@gmail.com>
2026-07-04 00:19:57 +0530

    Update API URL for production

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://data-analytics-portfolio-g9wi.onrender.com/api",
 });
 
 export default api;
-
-
